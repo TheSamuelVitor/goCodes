987e43ecdb8b44be81c843de4bf64250f481f1c5
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2023-06-01 09:35:40 -0300

    :truck: chore: adicionando arquitetura base do projeto

--- tapioca/main.go
+++ tapioca/main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
-	
-	var templates *template.Template
-
-	templates = template.Must(template.ParseGlob("./pages/*.html"))
-
-	server.GET("/", func(c *gin.Context) {
-
-		c.JSON(200, templates.ExecuteTemplate(c.Writer, "index.html", nil))
-
-	})
 
 	fmt.Println("Servidor rodando na porta 3000")
 	server.Run()
